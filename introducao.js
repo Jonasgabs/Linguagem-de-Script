75f94fe9ae90ca26b7426b345dfdf743b85e5c66
Jonas <jonas.sarmentof@gmail.com>
2024-10-16 16:22:33 -0300

    add introducao ao js

---
+++ introducao.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+console.log(0b1111);
+
+console.log(['sofia','sofia@gmail.com',18])
+
+console.log({
+    name:'sofia',
+    email:'sofia@gmail.com',
+    age: 18
+})
+
