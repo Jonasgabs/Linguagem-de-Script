b3308bbc166b37588d92810f581019f7525851f7
Jonasgsf <jonas.sarmentof@gmail.com>
2024-11-27 16:45:35 -0300

    adicionando Aula Web API

---
+++ web Api/index.js
@@ -0,0 +1,10 @@
+function Hello(name = 'user'){
+    console.log(`Script acionado papae, oi ${name}`);
+    console.log("vai se lascar toinnnnnnnn")
+    window.alert('sai fora')
+}
+
+function getCount(){
+    const count = prompt('fala o count');
+    window.alert(count)
+}
